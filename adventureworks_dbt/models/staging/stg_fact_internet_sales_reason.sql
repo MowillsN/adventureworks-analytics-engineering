23a8eaa683a34ab4702fd2d2552c681af0c4d9c3
unknown <nnamdiwilliams82@gmail.com>
2026-07-29 00:05:31 +0100

    Add sales_order_line_key surrogate key for Power BI bridge table relationship

--- adventureworks_dbt/models/staging/stg_fact_internet_sales_reason.sql
+++ adventureworks_dbt/models/staging/stg_fact_internet_sales_reason.sql
@@ -7,6 +7,7 @@ with source as (
 renamed as (
 
     select
+        concat(SalesOrderNumber, '-', cast(SalesOrderLineNumber as string)) as sales_order_line_key,
         SalesOrderNumber       as sales_order_number,
         SalesOrderLineNumber   as sales_order_line_number,
         SalesReasonKey         as sales_reason_key
